dce499cc78a9b1da69b361e4e5e7f1df16adf9d8
JezreilLeslie <lesliej@student.swosu.edu>
2015-10-07 09:42:02 -0500

    Added a print line

--- src/ClassNotesProject1.java
+++ src/ClassNotesProject1.java
@@ -1,11 +1,7 @@
-
 public class ClassNotesProject1 {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Class!");
 
 	}
 
